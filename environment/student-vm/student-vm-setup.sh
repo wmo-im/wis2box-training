a55987ac5ee75bacc854e0accff86b8ff5c19d66
Tom Kralidis <tomkralidis@gmail.com>
2023-04-06 06:10:12 -0400

    Update student-vm-setup.sh

--- environment/student-vm/student-vm-setup.sh
+++ environment/student-vm/student-vm-setup.sh
@@ -1,7 +1,7 @@
 # this is the wis2box-base-setup script to create the student-vm as used in the wis2box-training
-# using base-OS=Ubuntu 20.04 LTS (Focal)
+# using base OS: Ubuntu 20.04 LTS (Focal)
 
-# install the docker engine, python3 and docker-compose
+# install the Docker engine, Python 3 and Docker Compose
 sudo mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -12,7 +12,7 @@ sudo pip3 install pip --upgrade
 sudo pip3 install pyopenssl --upgrade
 sudo pip3 install docker-compose==1.29.2 requests==2.26.0 urllib3==1.26.0
 
-# install eccodes, pymetdecoder, csv2bufr, synop2bufr
+# install ecCodes, pymetdecoder, csv2bufr, synop2bufr
 ECCODES_VER=2.28.0
 ECCODES_DIR=/opt/eccodes
 PATH="$PATH;/opt/eccodes/bin"
@@ -40,10 +40,10 @@ sudo pip3 install https://github.com/wmo-im/pymetdecoder/archive/refs/tags/v0.1.
 sudo pip3 install https://github.com/wmo-im/csv2bufr/archive/refs/tags/v0.5.1.zip
 sudo pip3 install https://github.com/wmo-im/synop2bufr/archive/refs/tags/v0.3.2.zip
 
-# install various
+# install various utilities
 sudo apt-get -y install mosquitto-clients nano vim
 
-# install minio, pywis2-pubsubs, pywwiscat
+# install MinIO, pywis-pubsub, pywiscat
 sudo pip3 install minio==7.1.13
 sudo pip3 install pywis-pubsub
 sudo pip3 install https://github.com/wmo-im/pywiscat/archive/master.zip
