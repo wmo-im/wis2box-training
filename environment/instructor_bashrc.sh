0773feb378c6741885cfb2e37e781596f67429d5
Tom Kralidis <tomkralidis@gmail.com>
2023-06-13 15:46:28 -0400

    Update instructor_bashrc.sh

--- environment/instructor_bashrc.sh
+++ environment/instructor_bashrc.sh
@@ -2,5 +2,5 @@
 # execute the below to remove color formatting from instructor VM
 # in order to improve display when projecting
 
-export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[01;32m\]:\[\033[01;32m\]\w\[\033[01;32m\]\$ "
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[01;32m\]:\$ "
 unalias ls
